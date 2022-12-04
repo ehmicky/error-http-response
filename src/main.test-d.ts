50a9ffe63ab5fa9a8de88a8557532c3e0710b0fd
ehmicky <ehmicky@gmail.com>
2022-12-04 19:31:30 +0100

    Update type tests

--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -1,61 +1,66 @@
 import errorHttpResponse, { Options, HttpResponse } from 'error-http-response'
-import {
-  expectType,
-  expectAssignable,
-  expectNotAssignable,
-  expectError,
-} from 'tsd'
+import { expectType, expectAssignable, expectNotAssignable } from 'tsd'
 
 const error = new Error('')
 const httpResponse = errorHttpResponse(error)
 
-expectError(errorHttpResponse())
+// @ts-expect-error
+errorHttpResponse()
 expectAssignable<HttpResponse>(errorHttpResponse(true))
 
 errorHttpResponse(error, {})
 expectAssignable<Options>({})
 errorHttpResponse(error, undefined)
 expectNotAssignable<Options>(undefined)
-expectError(errorHttpResponse(error, true))
+// @ts-expect-error
+errorHttpResponse(error, true)
 expectNotAssignable<Options>(true)
-expectError(errorHttpResponse(error, { unknown: true }))
+// @ts-expect-error
+errorHttpResponse(error, { unknown: true })
 expectNotAssignable<Options>({ unknown: true })
 
 errorHttpResponse(error, { type: '' })
 expectAssignable<Options>({ type: '' })
-expectError(errorHttpResponse(error, { type: true }))
+// @ts-expect-error
+errorHttpResponse(error, { type: true })
 expectNotAssignable<Options>({ type: true })
 
 errorHttpResponse(error, { status: 200 })
 expectAssignable<Options>({ status: 200 })
-expectError(errorHttpResponse(error, { status: true }))
+// @ts-expect-error
+errorHttpResponse(error, { status: true })
 expectNotAssignable<Options>({ status: true })
 
 errorHttpResponse(error, { title: '' })
 expectAssignable<Options>({ title: '' })
-expectError(errorHttpResponse(error, { title: true }))
+// @ts-expect-error
+errorHttpResponse(error, { title: true })
 expectNotAssignable<Options>({ title: true })
 
 errorHttpResponse(error, { detail: '' })
 expectAssignable<Options>({ detail: '' })
-expectError(errorHttpResponse(error, { detail: true }))
+// @ts-expect-error
+errorHttpResponse(error, { detail: true })
 expectNotAssignable<Options>({ detail: true })
 
 errorHttpResponse(error, { instance: '' })
 expectAssignable<Options>({ instance: '' })
-expectError(errorHttpResponse(error, { instance: true }))
+// @ts-expect-error
+errorHttpResponse(error, { instance: true })
 expectNotAssignable<Options>({ instance: true })
 
 errorHttpResponse(error, { stack: '' })
 expectAssignable<Options>({ stack: '' })
-expectError(errorHttpResponse(error, { stack: true }))
+// @ts-expect-error
+errorHttpResponse(error, { stack: true })
 expectNotAssignable<Options>({ stack: true })
 
 errorHttpResponse(error, { extra: {} })
 expectAssignable<Options>({ extra: {} })
 errorHttpResponse(error, { extra: { prop: true } })
 expectAssignable<Options>({ extra: { prop: true } })
-expectError(errorHttpResponse(error, { extra: true }))
+// @ts-expect-error
+errorHttpResponse(error, { extra: true })
 expectNotAssignable<Options>({ extra: true })
 
 expectAssignable<HttpResponse>(httpResponse)
@@ -66,4 +71,5 @@ expectType<string>(httpResponse.detail)
 expectType<string | undefined>(httpResponse.instance)
 expectType<string>(httpResponse.stack)
 expectType<object | undefined>(httpResponse.extra)
-expectError(httpResponse.extra?.prop)
+// @ts-expect-error
+httpResponse.extra?.prop
